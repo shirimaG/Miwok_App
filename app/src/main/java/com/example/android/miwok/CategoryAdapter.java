484d587adfbf935a4e0f4163e108e9b0f2a3c17a
shirimaG <shirimagoodluck04@gmail.com>
2018-03-20 17:50:05 +0300

    Tab successful added now

--- app/src/main/java/com/example/android/miwok/CategoryAdapter.java
+++ app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -1,5 +1,6 @@
 package com.example.android.miwok;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -10,10 +11,18 @@ import android.support.v4.app.FragmentPagerAdapter;
 
 public class CategoryAdapter extends FragmentPagerAdapter{
 
-    public CategoryAdapter(FragmentManager fm) {
+    /** Context of the app */
+    private Context mContext;
+
+    public CategoryAdapter(Context context, FragmentManager fm) {
         super(fm);
+        mContext=context;
+
     }
 
+    /**
+     * Return the {@link Fragment} that should be displayed for the given page number.
+     */
     @Override
     public Fragment getItem(int position) {
 
@@ -28,9 +37,27 @@ public class CategoryAdapter extends FragmentPagerAdapter{
         }
     }
 
+    /**
+     * Return the total number of pages.
+     */
     @Override
     public int getCount() {
         //we need four pages thats why we return 4 as count
         return 4;
+    }
+    /***
+     * now we create this override method so can help us to create tabs
+     */
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return mContext.getString(R.string.category_numbers);
+        } else if (position == 1) {
+            return mContext.getString(R.string.category_family);
+        } else if (position == 2) {
+            return mContext.getString(R.string.category_colors);
+        } else {
+            return mContext.getString(R.string.category_phrases);
+        }
     }
 }
